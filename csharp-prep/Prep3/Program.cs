06dbb796b439f157d8c0a40b60609f8ef2322c1c
EskolaHJ <esk23003@byui.edu>
2023-09-22 15:44:30 -0600

    confusing

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        PlayGuessGame();
+    }
+    static void PlayGuessGame()
+    {
+        Random rand = new Random();
+        int magicNumber = rand.Next(1,20);
+
+        while (true)
+        {
+            Console.Write("What is your guess?");
+            int guess;
+            if (Int32.TryParse(Console.ReadLine(), out guess))
+            {
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+        }
     }
 }
