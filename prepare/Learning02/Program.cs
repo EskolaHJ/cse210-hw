0051838e09ae40f1ab1b93ba77a62a35b83fc01f
EskolaHJ <esk23003@byui.edu>
2023-09-28 17:41:24 -0600

    Still super confusing

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Carpet Insaller";
+        job1._company = "NomadFloors";
+        job1._startYear = 2015;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Dish Washer";
+        job2._company = "Cafe Luciano";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jacob Eskola";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
